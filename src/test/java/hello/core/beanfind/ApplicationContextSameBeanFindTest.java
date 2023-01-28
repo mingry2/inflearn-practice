14bfa95d4429784346befe9101aefdf74ed1eca0
minkyoung kim <minry20415@naver.com>
2023-01-28 15:19:40 +0900

    :sparkles: feat: Bean 조회기능

--- src/test/java/hello/core/beanfind/ApplicationContextSameBeanFindTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextSameBeanFindTest.java
@@ -1,6 +1,5 @@
 package hello.core.beanfind;
 
-import hello.core.AppConfig;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 import org.junit.jupiter.api.DisplayName;
@@ -9,9 +8,7 @@ import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import java.util.Map;
-
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -19,14 +16,12 @@ public class ApplicationContextSameBeanFindTest {
 
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SameBeanConfig.class);
 
-//    @Test
-//    @DisplayName("타입으로 조회 시 같은 타입이 둘 이상이 있으면 중복 오류가 발생된다.")
-//    void findBeanByTypeDuplicate() {
-//        MemberRepository bean = ac.getBean(MemberRepository.class);
-//
-//        assertThrows(NoUniqueBeanDefinitionException.class,
-//                () -> ac.getBean(MemberRepository.class));
-//    }
+    @Test
+    @DisplayName("타입으로 조회 시 같은 타입이 둘 이상이 있으면 중복 오류가 발생된다.")
+    void findBeanByTypeDuplicate() {
+        assertThrows(NoUniqueBeanDefinitionException.class,
+                () -> ac.getBean(MemberRepository.class));
+    }
 
     @Test
     @DisplayName("타입으로 조회 시 같은 타입이 둘 이상이 있으면 빈 이름을 지정하면 된다.")
